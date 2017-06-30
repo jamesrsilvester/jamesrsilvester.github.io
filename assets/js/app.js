9c3192d1959150cb9d6d4be082cb80815023b5f6
jamesrsilvester <jamesrsilvester@gmail.com>
2017-06-30 07:25:28 -0700

    added styling, jquery still broken

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
 // sanity check
-console.log("app.js linked.");
+console.log("linked.");
+
+// A $( document ).ready() block.
+$(document).ready(function() {
+    console.log( " and ready!" );
+
+let toggler = function() {
+  $('.toggleable').toggleClass('notBordered');
+}
+
+toggler();
+
+$('.clickable').click(toggler());
+
+});
